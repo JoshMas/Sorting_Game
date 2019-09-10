36f3c9133b56b7da6632f2b332a9a6527ece145c
Animator1 <39936619+Animator1@users.noreply.github.com>
2019-09-10 12:18:43 +1000

    Merged Yuran_Branch into master

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GotoGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void GotoCategoryScene()
+    {
+        SceneManager.LoadScene("CategoryScene");
+    }
+
+}
